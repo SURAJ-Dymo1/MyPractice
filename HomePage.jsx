312b4d113853f818fbabb3f6f4a3f2895d75ae19
chandan <chandan.bv@sciverse.co.in>
2025-01-16 18:42:24 +0530

    adde some random chnges

--- HomePage.jsx
+++ HomePage.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
 const HomePage=()=>{
-
+const callingRam=()=>{
+    alert("ram")
+}
     return(
+      <div>
+        <p>Jai Shree</p>
+    <div onClick={()=>callingRam()}>
+Sita ram
+</div>
+<div>RammmmmYUmmmmmmmmmmmmm</div>
+
+
+      </div>
       
+  
       
-      
-      
-      
-        <div>Rammmmmmmmmmmmmmmmmm</div>
     )
 }
