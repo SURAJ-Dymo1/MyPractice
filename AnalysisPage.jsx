1d10dffa1f5ee3cbdec3e283170a8de132ddf066
chandan <chandan.bv@sciverse.co.in>
2025-01-16 18:05:16 +0530

    Added History and Analysis Page

---
+++ AnalysisPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const AnalysisPage=()=>{
+
+    return(
+        <h1>Analysis Page</h1>
+    )
+}
---
+++ HistoryPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const HistoryPage=()=>{
+
+    return(
+        <h1>History Page</h1>
+    )
+}
