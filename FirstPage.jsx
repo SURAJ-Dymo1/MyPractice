01d6038e136add4c5dd5b07f77aa5647fb5c7135
chandan <chandan.bv@sciverse.co.in>
2025-01-16 18:16:52 +0530

    Added First and Second Page on First Branch

---
+++ FirstPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const FirstPage=()=>{
+
+    return(
+        <h1>First Page</h1>
+    )
+}
---
+++ SecondPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const SecondPage=()=>{
+
+    return(
+        <h1>Second Page</h1>
+    )
+}
